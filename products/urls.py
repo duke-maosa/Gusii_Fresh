36ad427c7ba5d2307a67280112e492978230be10
duke_maosa <dukemaosa75@gmail.com>
2024-06-06 16:20:44 -0400

    products

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.product_list, name='products'), 
-    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('', views.product_list, name='product_list'), 
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
--- search/views.py
+++ search/views.py
@@ -1,21 +1,31 @@
-# search/views.py
-
 from django.shortcuts import render
 from products.models import Product
-from account.models import CustomUser  # Import the CustomUser model from account app
+from account.models import CustomUser
+from django.db.models import Q
 
 def search(request):
-    query = request.GET.get('q')
+    query = request.GET.get('q', '')
+    sort_by = request.GET.get('sort_by', 'name')
+    order = request.GET.get('order', 'asc')
+
     if query:
         sellers = CustomUser.objects.filter(user_type='seller', username__icontains=query)
-        products = Product.objects.filter(name__icontains=query) | Product.objects.filter(description__icontains=query)
+        products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
         buyers = CustomUser.objects.filter(user_type='buyer', username__icontains=query)
+        
+        if order == 'desc':
+            sort_by = '-' + sort_by
+        products = products.order_by(sort_by)
+
         context = {
             'sellers': sellers,
             'products': products,
             'buyers': buyers,
             'query': query,
+            'sort_by': sort_by,
+            'order': order,
         }
     else:
         context = {}
+
     return render(request, 'search/search_results.html', context)
