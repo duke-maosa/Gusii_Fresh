28524a91237d3e48525c5390e31a49795fc29574
duke_maosa <dukemaosa75@gmail.com>
2024-06-08 08:25:58 -0400

    updated url

--- cart/urls.py
+++ cart/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove/<int:cart_item_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('update/<int:cart_item_id>/', views.update_cart, name='update_cart'),
+     path('view/', views.view_cart, name='view_cart'),
 ]
