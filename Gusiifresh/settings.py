91dbab13ab27221a0f96469a359a90cfc9113227
duke_maosa <dukemaosa75@gmail.com>
2024-06-13 06:41:02 -0400

    updated contact forms

--- Gusiifresh/settings.py
+++ Gusiifresh/settings.py
@@ -143,14 +143,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Define the path to your static files directories within each app
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    BASE_DIR / "static",
 ]
+
 
 
 
