427bc08cb5f45d3a9ed32ddc0d3cf6be0a29c0ed
duke_maosa <dukemaosa75@gmail.com>
2024-06-18 04:42:05 -0400

    set-up

--- Gusiifresh/settings.py
+++ Gusiifresh/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b&x^=vs)nh&@d6e2$2_7!w%edrem%x^s)z!a@7s24y5t7h%d2t'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
